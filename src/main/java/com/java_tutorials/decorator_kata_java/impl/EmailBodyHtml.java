df8beb47179b0954d78363cd41dea9359b97e3d4
Piercarlo Slavazza <p.slavazza@elibra.eu>
2021-03-29 13:36:58 +0200

    added hascode, equals, tostring methods to EmailBodyHtml and EmailBodyPlainText

--- src/main/java/com/java_tutorials/decorator_kata_java/impl/EmailBodyHtml.java
+++ src/main/java/com/java_tutorials/decorator_kata_java/impl/EmailBodyHtml.java
@@ -3,6 +3,8 @@ package com.java_tutorials.decorator_kata_java.impl;
 import com.java_tutorials.decorator_kata_java.EmailBody;
 import com.java_tutorials.decorator_kata_java.EmailBodyVisitor;
 
+import java.util.Objects;
+
 public class EmailBodyHtml implements EmailBody {
 
     private final String htmlBody;
@@ -17,5 +19,24 @@ public class EmailBodyHtml implements EmailBody {
 
     @Override public <T> T acceptEmailBodyVisitor(EmailBodyVisitor<T> emailBodyVisitor) {
 	return emailBodyVisitor.visit(this);
+    }
+
+    @Override public boolean equals(Object o) {
+	if (this == o)
+	    return true;
+	if (o == null || getClass() != o.getClass())
+	    return false;
+	EmailBodyHtml that = (EmailBodyHtml) o;
+	return Objects.equals(htmlBody, that.htmlBody);
+    }
+
+    @Override public int hashCode() {
+	return Objects.hash(htmlBody);
+    }
+
+    @Override public String toString() {
+	return "EmailBodyHtml{" +
+			"htmlBody='" + htmlBody + '\'' +
+			'}';
     }
 }
--- src/main/java/com/java_tutorials/decorator_kata_java/impl/EmailBodyPlainText.java
+++ src/main/java/com/java_tutorials/decorator_kata_java/impl/EmailBodyPlainText.java
@@ -3,6 +3,8 @@ package com.java_tutorials.decorator_kata_java.impl;
 import com.java_tutorials.decorator_kata_java.EmailBody;
 import com.java_tutorials.decorator_kata_java.EmailBodyVisitor;
 
+import java.util.Objects;
+
 public class EmailBodyPlainText implements EmailBody {
 
     private final String plainTextBody;
@@ -17,5 +19,24 @@ public class EmailBodyPlainText implements EmailBody {
 
     @Override public <T> T acceptEmailBodyVisitor(EmailBodyVisitor<T> emailBodyVisitor) {
 	return emailBodyVisitor.visit(this);
+    }
+
+    @Override public boolean equals(Object o) {
+	if (this == o)
+	    return true;
+	if (o == null || getClass() != o.getClass())
+	    return false;
+	EmailBodyPlainText that = (EmailBodyPlainText) o;
+	return Objects.equals(plainTextBody, that.plainTextBody);
+    }
+
+    @Override public int hashCode() {
+	return Objects.hash(plainTextBody);
+    }
+
+    @Override public String toString() {
+	return "EmailBodyPlainText{" +
+			"plainTextBody='" + plainTextBody + '\'' +
+			'}';
     }
 }
